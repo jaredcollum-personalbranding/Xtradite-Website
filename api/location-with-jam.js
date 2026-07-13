c72e9efb7035f1890c63dd16e60e73aaa0ddf479
jaredcollum-personalbranding <jaredcollum@gmail.com>
2026-07-13 23:22:00 +0100

    Attach live Jam metadata to routed pages

--- api/location-with-jam.js
+++ api/location-with-jam.js
@@ -1,21 +1,24 @@
 const locationHandler = require("./location");
 
-const marker = 'name="jam:team"';
+const teamMarker = 'name="jam:team"';
+const metadataMarker = 'src="/assets/js/jam-metadata.js"';
 const jamHead = `<meta name="jam:team" content="e8e1b81a-519b-40e4-9720-4d2182dbc6da" />
 <script type="module" src="https://js.jam.dev/recorder.js"></script>
-<script type="module" src="https://js.jam.dev/capture.js"></script>`;
+<script type="module" src="https://js.jam.dev/capture.js"></script>
+<script type="module" src="/assets/js/jam-metadata.js"></script>`;
+const metadataScript = `<script type="module" src="/assets/js/jam-metadata.js"></script>`;
 
 module.exports = async (req, res) => {
   const originalSend = res.send.bind(res);
 
   res.send = (body) => {
     const contentType = String(res.getHeader?.("Content-Type") || "");
-    if (
-      typeof body === "string" &&
-      /text\/html/i.test(contentType) &&
-      !body.includes(marker)
-    ) {
-      body = body.replace(/<\/head>/i, `${jamHead}</head>`);
+    if (typeof body === "string" && /text\/html/i.test(contentType)) {
+      if (!body.includes(teamMarker)) {
+        body = body.replace(/<\/head>/i, `${jamHead}</head>`);
+      } else if (!body.includes(metadataMarker)) {
+        body = body.replace(/<\/head>/i, `${metadataScript}</head>`);
+      }
     }
     return originalSend(body);
   };
